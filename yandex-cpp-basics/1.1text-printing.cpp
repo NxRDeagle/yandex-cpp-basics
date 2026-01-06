daadb245311dc752fc65e92fe20cfbcca989c3e6
Dmitriy <122107908+NxRDeagle@users.noreply.github.com>
2026-01-06 16:23:48 +0500

    Добавьте файлы проекта.

---
+++ yandex-cpp-basics/1.1text-printing.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+#ifdef ACTIVE_MAIN
+int main() {
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
+    std::cout << "  - is a better C\n";
+    std::cout << "  - supports data abstraction\n";
+    std::cout << "  - supports object-oriented programming\n";
+    std::cout << "  - supports generic programming.";
+    return 0;
+}
+#endif
+
+/*
+    Напишите программу, печатающую на экране первые строчки со страницы Бьярне Страуструпа про C++.
+    Не потеряйте парные пробелы в начале строк и переносы в конце строк.
+*/
